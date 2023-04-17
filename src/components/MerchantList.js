6b4250614a3eafcc5a14648fefcb2658c1cb0865
Anees Sahla <s17140@sci.pdn.ac.lk>
2023-04-18 05:04:16 +0530

    bug fixed

--- src/components/MerchantList.js
+++ src/components/MerchantList.js
@@ -1,9 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+
 import axios from 'axios';
 import './MerchantList.css';
 
+import Button from "@mui/material/Button";
+const baseApiUrl = "http://localhost:8080";
+
 const MerchantList = () => {
+  const history = useHistory();
+
   const [data, setData] = useState([]);
+  const [formData, setFormData] = useState({
+		search: "",
+	});
+
+  const handleInputChange = (event) => {
+		const { name, value } = event.target;
+		setFormData({ ...formData, [name]: value });
+	};
 
   useEffect(() => {
     axios.get('http://localhost:8080/merchant/get_all', {
@@ -19,15 +34,59 @@ const MerchantList = () => {
       });
   }, []);
 
+  const handleSubmit = (event) => {
+		event.preventDefault();
+    console.log(formData.search)
+		axios
+			.get(`${baseApiUrl}/merchant/search/${formData.search}`)
+			.then((response) => {
+				console.log(response.data.data);
+        setData(response.data.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+  const handleClick = (item) => {
+    console.log(item)
+    history.push('/userview', { data: item });
+  };
+
   return (
     <div className='merchantListBox'>
-    <h2>Merchants</h2>
+      <h1 className='heading'>ALL MERCHANTS</h1>
+
+      <div className='search'>
+      <form onSubmit={handleSubmit} className='seacrhForm'>
+      <input
+					type='text'
+								className='search-bar'
+								id='search'
+								name='search'
+								// aria-describedby='emailHelp'
+								placeholder='Search'
+								value={formData.search}
+								onChange={handleInputChange}
+				/>
+          <Button
+							type='submit'
+							variant='contained'
+							size='medium'
+							className='search-btn'
+							//onClick={attemptLogin}
+						>
+							Search
+					</Button>
+      </form>
+      </div>
+
       {data.map((item) => (
-        <div key={item.ID} className='merchantbox'>
+        <div key={item.ID} className='merchantbox' onClick={() => handleClick(item)}>
           
-          <h3>{item.Name}</h3>
-          <h4>{item.Category}</h4>
-          <h4>{item.Email}</h4>
+          <h3>Name of the Merchant : {item.Name}</h3>
+          <h4>Category : {item.Category}</h4>
+          <h4>Email : {item.Email}</h4>
         </div>
       ))}
     </div>
--- src/components/UserviewOfMerchant.js
+++ src/components/UserviewOfMerchant.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { useLocation, Link } from "react-router-dom";
+import map from '../img/map.jpg';
 
 function UserviewOfMerchant() {
 
+    const { data } = useLocation().state;
 
     const showSlot = () => {
         <div className='slotArea'>
@@ -11,15 +14,37 @@ function UserviewOfMerchant() {
     };
 
 	return (
-		<div>
-			<div className='queueArea'>
-				<Button>Queue 1</Button>
-			</div>
+		<div className='profile'>
+        <div className="header">
+            <h1>{data.Name}</h1>
+        </div>
 
-            <div className='dayArea'>
-                <Button onClick={showSlot}>Day</Button>
-            </div>
-		</div>
+        <div className='details'>
+            <h2>Merchant Profile</h2>
+            <p>Name: <span className='detail-answer'>{data.Name}</span></p>
+            <p>Email: <span className='detail-answer'>{data.Email}</span></p>
+            <p>Category: <span className='detail-answer'>{data.Category}</span></p>
+        </div>
+
+
+            <div className='map'>
+                {/* <Map address={address} /> */}
+                <img src={map} alt="map" />
+          </div>
+
+          <div className='scheduleToken'>
+            <Link exact to='/merchantscheduletoken'>
+            <Button
+                type='submit'
+                variant="contained" 
+                size="large" 
+                className='schedule-btn'>
+                    
+                Schedule Token
+                </Button>
+            </Link>
+          </div>
+    </div>
 	);
 }
 
